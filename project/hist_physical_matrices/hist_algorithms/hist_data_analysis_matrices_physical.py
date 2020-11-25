460aef45210a227a0f991e141d6a6b158818a785
juanhenao21 <juanhenao21@gmail.com>
2020-11-25 16:44:54 +0100

    Implement different time interval correlations

--- project/hist_physical_matrices/hist_algorithms/hist_data_analysis_matrices_physical.py
+++ project/hist_physical_matrices/hist_algorithms/hist_data_analysis_matrices_physical.py
@@ -7,7 +7,6 @@ This script requires the following modules:
     * os
     * pickle
     * typing
-    * numpy
     * pandas
 
 The module contains the following functions:
@@ -25,7 +24,6 @@ import os
 import pickle
 from typing import List, Tuple
 
-import numpy as np  # type: ignore
 import pandas as pd  # type: ignore
 
 import hist_data_tools_matrices_physical
@@ -45,6 +43,10 @@ def hist_fx_returns_year_physical_data(fx_pairs: List[str], year: str) -> None:
 
     weeks: Tuple[str, ...] = hist_data_tools_matrices_physical.hist_weeks()
 
+    function_name: str = hist_fx_returns_year_physical_data.__name__
+    hist_data_tools_matrices_physical \
+        .hist_function_header_print_data(function_name, year, 'returns')
+
     try:
 
         fx_df_concat: pd.DataFrame = pd.DataFrame()
@@ -53,20 +55,19 @@ def hist_fx_returns_year_physical_data(fx_pairs: List[str], year: str) -> None:
         for fx_pair in fx_pairs:
             # Load first week data
             fx_data: pd.DataFrame = pickle.load(open(
-                            f'../../hist_data/physical_basic_data_{year}/hist'
-                            + f'_fx_physical_basic_data/{fx_pair}/hist_fx'
-                            + f'_physical_basic_data_{fx_pair}_w01.pickle',
-                            'rb'))
+                f'../../hist_data/physical_basic_data_{year}/hist_fx_physical'
+                + f'_basic_data/{fx_pair}/hist_fx_physical_basic_data'
+                + f'_{fx_pair}_w01.pickle', 'rb'))
+
             fx_series_concat = fx_data['Returns']
 
             week: str
             for week in weeks[1:]:
                 # Load data
-                fx_data= pickle.load(open(
-                            f'../../hist_data/physical_basic_data_{year}/'
-                            + f'hist_fx_physical_basic_data/{fx_pair}/hist'
-                            + f'_fx_physical_basic_data_{fx_pair}_w{week}'
-                            + f'.pickle', 'rb'))
+                fx_data = pickle.load(open(
+                    f'../../hist_data/physical_basic_data_{year}/hist_fx'
+                    + f'_physical_basic_data/{fx_pair}/hist_fx_physical_basic'
+                    + f'_data_{fx_pair}_w{week}.pickle', 'rb'))
 
                 fx_series_concat = pd.concat([fx_series_concat,
                                               fx_data['Returns']])
@@ -108,10 +109,15 @@ def hist_fx_correlations_physical_data(year: str, interval: str) -> None:
     """Computes the correlation matrix in an interval of time.
 
     :param year: string of the year to be analyzed (i.e. '2016').
-    :param interval:
+    :param interval: string of the interval to be analyzed (i.e. 'week',
+     'month', 'quarter', 'year')
     :return: None -- The function saves the data in a file and does not return
      a value.
     """
+
+    function_name: str = hist_fx_correlations_physical_data.__name__
+    hist_data_tools_matrices_physical \
+        .hist_function_header_print_data(function_name, year, 'corr')
 
     try:
         # Load data
@@ -120,9 +126,52 @@ def hist_fx_correlations_physical_data(year: str, interval: str) -> None:
                         + f'_matrices_physical_data/hist_fx_returns_matrices'
                         + f'_physical_data_{year}.pickle', 'rb'))
 
-        corr = fx_returns.corr()
+        freq: str
+        periods: int
+        if interval == 'week':
+            freq = 'W'
+            periods = 52
+        elif interval == 'month':
+            freq = 'MS'
+            periods = 12
+        else:
+            freq = 'QS'
+            periods = 4
+
+        if interval == 'year':
+            corr: pd.DataFrame = fx_returns.corr()
+            hist_data_tools_matrices_physical \
+                .hist_save_data(corr, year, interval, '01')
+
+        else:
+            time_int: pd.DatetimeIndex = \
+                pd.date_range(f'{year}-01-01', periods=periods, freq=freq)
+
+            t_idx: int
+            t_idx_str: str
+            for t_idx in range(1, periods):
+                corr = fx_returns[time_int[t_idx - 1]: time_int[t_idx]].corr()
+
+                if t_idx < 10:
+                    t_idx_str = f'0{t_idx}'
+                else:
+                    t_idx_str = f'{t_idx}'
+
+                hist_data_tools_matrices_physical \
+                    .hist_save_data(corr, year, interval, t_idx_str)
+
+            corr = fx_returns[time_int[-1]:].corr()
+
+            if t_idx < 10:
+                t_idx_str = f'0{t_idx + 1}'
+            else:
+                t_idx_str = f'{t_idx + 1}'
+
+            hist_data_tools_matrices_physical \
+                .hist_save_data(corr, year, interval, t_idx_str)
 
         del fx_returns
+        del corr
 
     except FileNotFoundError as error:
         print('No data')
