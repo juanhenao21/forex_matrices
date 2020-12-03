b95f51e2100a5916f55092e8774f4d5077f04285
juanhenao21 <juanhenao21@gmail.com>
2020-12-03 20:01:20 +0100

    Implement eigenvectors for different time intervals

--- project/hist_physical_eigenvectors/hist_algorithms/hist_data_analysis_eigenvectors_physical.py
+++ project/hist_physical_eigenvectors/hist_algorithms/hist_data_analysis_eigenvectors_physical.py
@@ -47,7 +47,6 @@ def hist_fx_eigenvectors_physical_data(year: str, interval: str) -> None:
 
     try:
 
-
         freq: str
         periods: int
         if interval == 'week':
@@ -61,68 +60,69 @@ def hist_fx_eigenvectors_physical_data(year: str, interval: str) -> None:
             periods = 4
 
         if interval == 'year':
+
             # Load data
             fx_corr: pd.DataFrame = pickle.load(open(
                             f'../../hist_data/matrices_physical_{year}/hist_fx'
                             + f'_matrices_physical_data/hist_fx_corr_physical'
                             + f'_data_{year}_int_{interval}_01.pickle', 'rb'))
 
-            print('Correlation Matrix')
-            print()
-            print(fx_corr)
-            print()
             eig_val: np.ndarray
             eig_vec: np.ndarray
             eig_val, eig_vec = np.linalg.eig(fx_corr)
-            # print(eig_val)
 
-            eig_vec_sort = eig_vec[:, np.argsort(eig_val)[::-1]]
-            print()
-            print('Eigenvectors sorted according to the largest Eigenvalues')
-            print()
-            print(eig_vec_sort)
-            # eig_vec_sort_df = pd.DataFrame(data=eig_vec_sort,
-            #                                index=fx_corr.columns,
-            #                                columns=fx_corr.columns)
-            # print(eig_vec_sort_df)
+            # Sort the eigenvectors according to the positions of the
+            # eigenvalues from larger to smaller
+            eig_vec_sort: np.ndarray = eig_vec[:, np.argsort(eig_val)[::-1]]
 
-            # hist_data_tools_eigenvectors_physical \
-            #     .hist_save_data(corr, year, interval, '01')
+            # Sort the columns according to the positions of the eigenvalues
+            cols_eig_vec_sort: pd.Index = fx_corr \
+                .columns[np.argsort(eig_val)][::-1]
+            # DataFrame with the sorted eigenvectors
+            eig_vec_sort_df: pd.DataFrame = pd.DataFrame(data=eig_vec_sort,
+                                                   columns=cols_eig_vec_sort)
+
+            hist_data_tools_eigenvectors_physical \
+                .hist_save_data(eig_vec_sort_df, year, interval, '01')
 
         else:
-            # Load data
-            fx_returns: pd.DataFrame = pickle.load(open(
-                            f'../../hist_data/matrices_physical_{year}/hist_fx'
-                            + f'_matrices_physical_data/hist_fx_corr_physical_data'
-                            + f'_{year}_int_{interval}_{t_idx}'
-                            + f'_physical_data_{year}.pickle', 'rb'))
             time_int: pd.DatetimeIndex = \
                 pd.date_range(f'{year}-01-01', periods=periods, freq=freq)
 
             t_idx: int
             t_idx_str: str
-            for t_idx in range(1, periods):
-                corr = fx_returns[time_int[t_idx - 1]: time_int[t_idx]].corr()
+            for t_idx in range(1, periods + 1):
 
                 if t_idx < 10:
                     t_idx_str = f'0{t_idx}'
                 else:
                     t_idx_str = f'{t_idx}'
 
-                hist_data_tools_matrices_physical \
-                    .hist_save_data(corr, year, interval, t_idx_str)
+                # Load data
+                fx_corr: pd.DataFrame = pickle.load(open(
+                                f'../../hist_data/matrices_physical_{year}/hist_fx'
+                                + f'_matrices_physical_data/hist_fx_corr_physical'
+                                + f'_data_{year}_int_{interval}_{t_idx_str}.pickle', 'rb'))
 
-            corr = fx_returns[time_int[-1]:].corr()
+                eig_val, eig_vec = np.linalg.eig(fx_corr)
 
-            if t_idx < 10:
-                t_idx_str = f'0{t_idx + 1}'
-            else:
-                t_idx_str = f'{t_idx + 1}'
+                # Sort the eigenvectors according to the positions of the
+                # eigenvalues from larger to smaller
+                eig_vec_sort: np.ndarray = eig_vec[:, np.argsort(eig_val)[::-1]]
 
-            hist_data_tools_matrices_physical \
-                .hist_save_data(corr, year, interval, t_idx_str)
+                # Sort the columns according to the positions of the eigenvalues
+                cols_eig_vec_sort: pd.Index = fx_corr \
+                    .columns[np.argsort(eig_val)][::-1]
+                # DataFrame with the sorted eigenvectors
+                eig_vec_sort_df: pd.DataFrame = pd.DataFrame(data=eig_vec_sort,
+                                                    columns=cols_eig_vec_sort)
+
+                hist_data_tools_eigenvectors_physical \
+                    .hist_save_data(eig_vec_sort_df, year, interval, t_idx_str)
 
         del fx_corr
+        del eig_vec_sort
+        del eig_vec_sort_df
 
     except FileNotFoundError as error:
         print('No data')
@@ -139,8 +139,6 @@ def main() -> None:
 
     :return: None.
     """
-
-    hist_fx_eigenvectors_physical_data('2019', 'year')
 
 # -----------------------------------------------------------------------------
 
